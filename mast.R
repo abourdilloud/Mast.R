4e864ad1abe46218858174636a6ed9ffd870e0f4
audrey.bourdilloud@outlook.com <audreybourdilloud@macmiam.local>
2017-12-14 23:26:20 +0100

    ignore mast.R

--- mast.R
+++ mast.R
@@ -1,6 +1,4 @@
-setwd("~/UNIGE/Stats & Proba/DAP")
-
-require(seqinr)
+library(seqinr)
 
 path = "./data"
 
@@ -8,10 +6,13 @@ listfasta <- list()
 list.files(path)
 
 
-test <- read.fasta("fasta_test.fasta")
+test <- read.fasta("data/aa_part.fasta")
 gc <- c()
 for(i in 1:length(test)){
   gc[i] <- summary(test[[i]])$GC
 }
 
 boxplot(gc)
+test$`3`
+
+attr(test[[1]],"name")
